f5445386d5ccf47c7a82b29812cf658e851cf277
Dominik Mrajca <racingpark54@gmail.com>
2020-01-27 16:12:25 +0100

    Początkowe poprawki

--- GameUI/MainWindow.xaml.cs
+++ GameUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GameLibrary;
 
 namespace GameUI
 {
@@ -20,6 +21,8 @@ namespace GameUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Game game;
+
         public MainWindow()
         {
             InitializeComponent();
